20c8739f049cdfa9e9f970e97b46c7c4261ed0db
Migalhaz <miguel.chagas614@gmail.com>
2025-01-08 22:43:18 -0300

    Update: InputManager, GameSceneManager, Bootstrap and Observer

---
+++ CorreMachoProject/Assets/Scripts/GameSystem/Bootstraper.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameSystem
+{
+    public class Bootstraper : MonoBehaviour
+    {
+        [SerializeField, Min(0)] int m_firstSceneIndex;
+
+        private void Awake()
+        {
+            Bootstrap();
+        }
+
+        void Bootstrap()
+        {
+            InputManager.ProvideInstance();
+            GameScenesManager.ProvideInstance().LoadSingleScene(m_firstSceneIndex);
+        }
+    }
+}
